415e9666489e6423132642934d08df537c5faa65
bradencarlson <bradenjcarlson@live.com>
2021-11-20 21:21:28 -0700

    updated the main.cpp file, includeing an example of file handling.

--- main.cpp
+++ main.cpp
@@ -3,15 +3,22 @@
 //
 
 #include <iostream>
+#include <fstream>
 #include "Car.h"
 #include "Customer.h"
 
 void customerDriver();
 void carDriver();
 
+
 int main(){
-    customerDriver();
-    carDriver();
+    std::fstream file;
+    file.open("cars.txt",std::ios::in);
+    if (file.is_open()) {
+       std::string line;
+       std::getline(file, line);
+       std::cout << line << std::endl;
+    }
 }
 
 // Test code for customer object
@@ -22,6 +29,6 @@ void customerDriver() {
 
 // test code for car object
 void carDriver() {
-    Car car{"Kia","forte", "2018", "K12341823", 18000};
+    Car car{"Ford","F150", "2025", "K12341823", 45000};
     std::cout << car.getMake() << " " << car.getModel() << " " << car.getYear() << " " << car.getVin() << " " << car.getPrice() << std::endl;
 }
